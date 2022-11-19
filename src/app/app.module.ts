464de924db5650037b327bb3b6bfec507bdfd2b2
Shawn Long <shawnlong@Shawns-MacBook-Pro.local>
2022-11-19 15:41:44 -0800

    added shipping data retrieval to cart servicec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { HttpClientModule } from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -7,6 +9,7 @@ import { ProductListComponent } from './product-list/product-list.component'
 import { TopBarComponent } from './top-bar/top-bar.component'
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component'
 import { ProductDetailsComponent } from './product-details/product-details.component'
+import { CartComponent } from './cart/cart.component'
 
 @NgModule({
   declarations: [
@@ -14,11 +17,14 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ProductListComponent,
     TopBarComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,11 +1,22 @@
+import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Product } from '../products'
+import { Observable } from 'rxjs'
+
+interface shippingPrice {
+  type: string
+  price: number
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   items: Product[] = []
+
+  constructor (
+    private readonly http: HttpClient
+  ) { }
 
   addToCart (product: Product): void {
     this.items.push(product)
@@ -18,5 +29,9 @@ export class CartService {
   clearCart (): Product[] {
     this.items = []
     return this.items
+  }
+
+  getShippingPrices (): Observable<shippingPrice[]> {
+    return this.http.get<shippingPrice[]>('/assets/shipping.json')
   }
 }
